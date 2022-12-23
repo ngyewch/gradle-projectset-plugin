939230a8b7f5d0dea25bcc7b43076c9cb2d3c9ca
Nick Ng <ngyewch@gmail.com>
2022-12-23 15:43:11 +0800

    chore: updated plugins

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
     `java-gradle-plugin`
     `maven-publish`
-    id("ca.cutterslade.analyze") version "1.9.0"
     id("com.asarkar.gradle.build-time-tracker") version "4.3.0"
-    id("com.github.ben-manes.versions") version "0.42.0"
-    id("com.gradle.plugin-publish") version "0.21.0"
+    id("com.autonomousapps.dependency-analysis") version "1.18.0"
+    id("com.github.ben-manes.versions") version "0.44.0"
+    id("com.gradle.plugin-publish") version "1.1.0"
     id("io.github.ngyewch.git-describe") version "0.2.0"
     id("se.ascp.gradle.gradle-versions-filter") version "0.1.16"
 }
